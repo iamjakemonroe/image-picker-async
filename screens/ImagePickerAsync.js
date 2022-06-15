ccf0ac3ded97fdc1cc7c13e988984047c3048fc0
Monroe <iamjakemonroe@gmail.com>
2022-06-15 03:33:02 +0100

    ImageAsync

---
+++ screens/ImagePickerAsync.js
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from "react";
+import {
+  Image,
+  View,
+  Platform,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import { Text, Button } from "react-native-elements";
+import * as ImagePicker from "expo-image-picker";
+
+export default function ImagePickerAsync({ navigation }) {
+  const [image, setImage] = useState(null);
+
+  const addImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
+  return (
+    <View style={{ alignItems: "center" }}>
+      <View style={imageUploaderStyles.container}>
+        {image && (
+          <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
+        )}
+
+        <View style={imageUploaderStyles.uploadBtnContainer}>
+          <TouchableOpacity
+            onPress={addImage}
+            style={imageUploaderStyles.uploadBtn}
+          >
+            <Text>{image ? "Edit" : "Upload"} Image</Text>
+            <AntDesign name="camera" size={20} color="black" />
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View style={{ height: 150 }}></View>
+    </View>
+  );
+}
+
+const imageUploaderStyles = StyleSheet.create({
+  buttonView: {
+    marginBottom: 2,
+    color: "red",
+  },
+  button: {
+    width: 100,
+  },
+  container: {
+    elevation: 2,
+    height: 200,
+    width: 200,
+    backgroundColor: "#efefef",
+    position: "relative",
+    top: 50,
+    borderRadius: 999,
+    overflow: "hidden",
+  },
+  uploadBtnContainer: {
+    opacity: 0.7,
+    position: "absolute",
+    right: 0,
+    bottom: 0,
+    backgroundColor: "lightgrey",
+    width: "100%",
+    height: "25%",
+  },
+  uploadBtn: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
